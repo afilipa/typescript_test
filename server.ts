0e41b117b4b6b7395f22546aad71d1d3769a4d37
Ana Teixeira <afilipa_teixeira@hotmail.com>
2018-12-05 00:05:33 +0000

    breaking news: new structure of code| new protections for missing data given by user | case insensitivity for location search in DB + search by locations containing the given str

--- server.ts
+++ server.ts
@@ -4,12 +4,18 @@ const request = require('request-promise');
 
 // Limit the amount of debugging of SQL expressions
 const trimLogsSize: number = 200;
-/* command-line processing */
+
+/* ** command - line processing **
+use cases:
+  --perLoc Lisbon -> gives users in Lisbon * /
+  --lisbon -> something like a shotcut for last command example ** just for Lisbon **
+  --stats -> gives the number of users per location in lovelyDB
+  --user afilipa -> inserts the given user in lovelyDB */
 const optionDefinitions = [
-  { name: 'perLoc', alias: 'L', type: String }, /* e.g: --perLoc Lisbon -> gives users in Lisbon*/
-  { name: 'lisbon', alias: 'l', type: Boolean }, /* e.g: --lisbon  -> something like a shotcut for last command example **just for Lisbon** */
-  { name: 'stats', alias: 's', type: Boolean }, /* e.g: --stats -> gives the number of users per location in lovelyDB */
-  { name: 'user', alias: 'u', type: String }  /* e.g: --user afilipa -> inserts the given user in lovelyDB */
+  { name: 'perLoc', alias: 'L', type: String },
+  { name: 'lisbon', alias: 'l', type: Boolean },
+  { name: 'stats', alias: 's', type: Boolean },
+  { name: 'user', alias: 'u', type: String }
 ]
 const commandLineArgs = require('command-line-args');
 const optionsCLI = commandLineArgs(optionDefinitions);
@@ -60,62 +66,100 @@ interface GithubUsers {
   location: string
 };
 
+/* creates table IF NOT EXISTS already */
+/* introduces a primary key and unique constraint on table creation */
+db.none(`CREATE TABLE IF NOT EXISTS github_users ( 
+  id BIGSERIAL PRIMARY KEY NOT NULL,
+  login TEXT NOT NULL UNIQUE,
+  name TEXT,
+  company TEXT,
+  location TEXT)
+`)
+  .then(() => console.log("\nWe have database! Let's do lovely things now ;)"))
+  .then(() => (!Object.keys(optionsCLI).length || commandErrors()) ? process.exit(0) : processOptions());
 
-if (optionsCLI.lisbon) {
-  UsersInLocation('Lisbon');
-}
-if (optionsCLI.perLoc) {
-  UsersInLocation(optionsCLI.perLoc);
-}else if (optionsCLI.perLoc != undefined) {
-  console.log("\n**ALERT** You forgot (or you are kidding me :P) your lovely location to search for");
-}
-if (optionsCLI.stats) {
-  UsersPerLocation();
-}
 
-if (optionsCLI.user) {
-      request({
-        uri: `https://api.github.com/users/${optionsCLI.user}`,
-        headers: {
-          'User-Agent': 'Request-Promise'
-        },
-        json: true
-      })
-    .then((data: GithubUsers) =>
-      db.one(`INSERT INTO github_users (login, name, company, location) VALUES ($[login], $[name], $[company], $[location]) RETURNING id`, data)
-    ).then(({ id }) => console.log(id));
-} else if (optionsCLI.user != undefined) {
-  console.log("\n**ALERT** I know you are testing me, but come on.. Insert a parameter with the user ;)");
-}
+const keys = Object.keys(optionsCLI);
 
-  /* creates table IF NOT EXISTS already */
-  /* introduces a primary key and unique constraint on table creation */
-  db.none(`CREATE TABLE IF NOT EXISTS github_users ( 
-    id BIGSERIAL PRIMARY KEY NOT NULL,
-    login TEXT NOT NULL UNIQUE,
-    name TEXT,
-    company TEXT,
-    location TEXT)
-  `);
+/* check errors in given commands */
+function commandErrors() {
+  for (let i in keys) {
+    if (optionsCLI[keys[i]] == null) {
+      console.log("The given option --" + keys[i] + " has its parameter missing");
+      return true;
+    }
+  }
+  return false;
+}
+/* process the commands given */
+function processOptions() {
+  for (let i in keys) {
+    switch (keys[i]) {
+      case 'lisbon':
+        UsersInLocation('Lisbon', i);
+        break;
+      case 'perLoc':
+        UsersInLocation(optionsCLI.perLoc, i);
+        break;
+      case 'stats':
+        StatsPerLocation();
+        break;
+      case 'user':
+        request({
+          uri: `https://api.github.com/users/${optionsCLI.user}`,
+          headers: {
+            'User-Agent': 'Request-Promise'
+          },
+          json: true
+        })
+          .then((data: GithubUsers) =>
+            db.one(`INSERT INTO github_users (login, name, company, location) 
+          VALUES ($[login], $[name], $[company], $[location]) RETURNING id`, data)
+          ).then(({ id }) => console.log(id))
+          .then(() => (keys.indexOf('user') == keys.length - 1) ? process.exit(0) : "");
+        break;
+      default:
+        break;
+    }
+  }
+}
 
 /* returns a list of users in given location */
-function UsersInLocation(loc) {
-  return db.any('select exists(select 1 from information_schema.tables where table_name=\'github_users\')').then(data => data.map(row => row.exists
-  )[0]).then(bool => (bool) ?
-    db.any('select * from github_users where location = $1', [loc])
-      .then(data => { console.log("\nGitHub Users living in " + loc); console.log(`\n|login | name | company|`); data.forEach((row) => console.log("|" + row.login + " | " + row.name + " | " + row.company + "|")); }).then(() => process.exit(0))
-    : console.log("\nI apologize but there is no way to search for data, you didn't create the table yet")
-  );
+function UsersInLocation(loc, indexCommand) {
+  return db.any(`select exists(select 1 
+    from information_schema.tables 
+    where table_name='github_users')`)
+    .then(data => data.map(row => row.exists)[0])
+    .then(bool => (bool) ?
+      db.any(`select * from github_users where lower(location) like lower(concat('%', $1,'%'))`, [loc])
+        .then(data => {
+          if (data.length) {
+            console.log("\nGitHub Users living in " + loc);
+            console.log(`\n|login | name | company| Location`);
+            data.forEach((row) =>
+              console.log("|" + row.login + " | " + row.name + " | " + row.company + "|" + row.location));
+          } else {
+            console.log("\nOoops! No users in " + loc);
+          }
+        })
+
+      : console.log("\nThere is no way to search for data, you didn't create the table yet :P")
+    ).then(() => (indexCommand == keys.length - 1) ? process.exit(0) : "");
 
 }
 
 /* returns the count of users by location */
-function UsersPerLocation() {
-  return db.any('select exists(select 1 from information_schema.tables where table_name=\'github_users\')').then(data => data.map(row => row.exists
-  )[0]).then(bool => (bool) ? db.any('select location, count(*) from github_users group by location', [])
-    .then(data => { console.log("\nNext stats of ppl/location\n"); console.log(`|Location  -> #UsersIn |`); data.forEach((row) => console.log("|" + row.location + " -> " + row.count + " |")); })
-    : console.log("\nWe Apologize but there is no way to search for data yet")
-  );
+function StatsPerLocation() {
+  return db.any('select exists(select 1 from information_schema.tables where table_name=\'github_users\')')
+    .then(data => data.map(row => row.exists)[0])
+    .then(bool => (bool) ? db.any('select location, count(*) from github_users group by location', [])
+      .then(data => {
+        console.log("\nNext stats of ppl/location\n");
+        console.log(`|Location  -> #UsersIn |`);
+        data.forEach((row) => console.log("|" + row.location + " -> " + row.count + " |"));
+      })
+      : console.log("\nWe Apologize but there is no way to search for data yet")
+    ).then(() => (keys.indexOf('stats') == keys.length - 1) ? process.exit(0) : "");
 }
 
 
